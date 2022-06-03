15d9bce6d50409246c6062222b76f3c73c9159a0
Matthew <mattj20@gmail.com>
2022-06-02 19:54:32 -0700

    commit tree dfc9f2b75354a654f73c38106b6b5fc6390b9565

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(know)
+        @knowledge << know
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
